aa67f480013b5589551facecf6930b4d1c0c0748
himanshulodha2002 <himanshulodha2002@gmail.com>
2023-10-29 11:55:11 +0530

    splited it merged it

--- graph.cpp
+++ graph.cpp
@@ -1,48 +1,38 @@
 #include "graph.h"
-#include <iostream>
-#include <fstream>
-#include <random>
-#include <ctime>
 
-using namespace std;
-
-Edge::Edge(Vertex * dest) {
-    this->dest = dest;
-    this->weight = 1;
-}
+Edge::Edge(Vertex* dest) : dest(dest), weight(1) {}
 
 void Edge::increment_weight() {
     weight++;
 }
 
-Vertex::Vertex(std::string value) {
-    this->value = value;
-}
+Vertex::Vertex(std::string value) : value(value) {}
 
-void Vertex::add_edge(Vertex * vertex) {
+void Vertex::add_edge(Vertex* vertex) {
     if (edges.find(vertex->value) == edges.end()) {
         edges[vertex->value] = new Edge(vertex);
     }
 }
 
-void Vertex::increment_edge(Vertex * vertex) {
+void Vertex::increment_edge(Vertex* vertex) {
     add_edge(vertex);
     edges[vertex->value]->increment_weight();
 }
 
 void Vertex::calculate_probabilities() {
     int sum = 0;
-    for (auto const& pair: edges) {
+    for (auto const& pair : edges) {
         sum += pair.second->weight;
     }
 
-    for (auto const& pair: edges) {
-        double probability = (double) pair.second->weight / sum;
+    for (auto const& pair : edges) {
+        double probability = static_cast<double>(pair.second->weight) / sum;
         probabilities[pair.first] = probability;
     }
+
 }
 
-Vertex * Graph::get_vertex(std::string value) {
+Vertex* Graph::get_vertex(std::string value) {
     if (vertices.find(value) == vertices.end()) {
         vertices[value] = new Vertex(value);
     }
@@ -50,106 +40,28 @@ Vertex * Graph::get_vertex(std::string value) {
 }
 
 void Graph::add_edge(std::string v1, std::string v2) {
-    Vertex * vertex1 = get_vertex(v1);
-    Vertex * vertex2 = get_vertex(v2);
+    Vertex* vertex1 = get_vertex(v1);
+    Vertex* vertex2 = get_vertex(v2);
     vertex1->increment_edge(vertex2);
 }
 
 void Graph::generate_probability_mappings() {
-    for (auto const& pair: vertices) {
+    for (auto const& pair : vertices) {
         pair.second->calculate_probabilities();
     }
 }
 
-Vertex * Graph::get_next_word(Vertex * word) {
+Vertex* Graph::get_next_word(Vertex* word) {
     std::map<std::string, double> probabilities = word->probabilities;
 
-    double probability = (double) rand() / RAND_MAX;
+    double probability = static_cast<double>(rand()) / RAND_MAX;
 
     double cumulative_probability = 0.0;
-    for (auto const& pair: probabilities) {
+    for (auto const& pair : probabilities) {
         cumulative_probability += pair.second;
         if (cumulative_probability >= probability) {
             return vertices[pair.first];
         }
     }
     return nullptr;
-}
-
-std::vector<std::string> get_words_from_text(std::string text_path) {
-    std::ifstream file(text_path);
-    std::string word;
-    std::vector<std::string> words;
-    while (file >> word) {
-        words.push_back(word);
-    }
-    file.close();
-    return words;
-}
-
-vector<string> remove_punctuation(vector<string> words) {
-    vector<string> no_punct;
-    for (string word : words) {
-        string no_punct_word = "";
-        for (char c : word) {
-            if (isalpha(c)) {
-                no_punct_word += tolower(c);
-            }
-        }
-        no_punct.push_back(no_punct_word);
-    }
-    return no_punct;
-} 
-
-Graph make_graph(vector<string> words) {
-    Graph g;
-    Vertex * prev_word = nullptr;
-    // for each word
-    for (string word : words) {
-        // check that word is in graph, and if not then add it
-        Vertex * word_vertex = g.get_vertex(word);
-
-        // if there was a previous word, then add an edge if does not exist
-        // if exists, increment weight by 1
-        if (prev_word) {
-            // check if edge exists from previous word to current word
-            prev_word->increment_edge(word_vertex);
-        }
-
-        prev_word = word_vertex;
-    }
-
-    g.generate_probability_mappings();
-
-    return g;
-}
-
-vector<string> compose(Graph g, vector<string> words, int length=50) {
-    vector<string> composition;
-    Vertex * word = g.get_vertex(words[rand() % words.size()]);
-    for (int i = 0; i < length; i++) {
-        composition.push_back(word->value);
-        word = g.get_next_word(word);
-        if (!word) {
-            word = g.get_vertex(words[rand() % words.size()]);
-        }
-    }
-
-    return composition;
-}
-
-int main() {
-    srand(time(nullptr));
-    vector<string> words1 = get_words_from_text("hp_sorcerer_stone.txt");
-    vector<string> words = remove_punctuation(words1);
-
-    Graph g = make_graph(words);
-    vector<string> composition = compose(g, words, 100);
-    
-    for(string word : composition) {
-        cout << word << " ";
-    }
-    cout << endl;
-
-    return 0;
 }
--- graph.h
+++ graph.h
@@ -1,46 +1,43 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-
 #include <string>
-#include <map>
 #include <vector>
-using namespace std;
+#include <map>
+
 class Vertex;
 
 class Edge {
 public:
-    Vertex * dest;
+    Vertex* dest;
     int weight;
 
-    Edge(Vertex * dest);
+    Edge(Vertex* dest);
+
     void increment_weight();
 };
 
 class Vertex {
 public:
-    string value;
-    map<string, Edge *> edges;
-    map<string, double> probabilities;
+    std::string value;
+    std::map<std::string, Edge*> edges;
+    std::map<std::string, double> probabilities;
 
-    Vertex(string value);
-    void add_edge(Vertex * vertex);
-    void increment_edge(Vertex * vertex);
+    Vertex(std::string value);
+
+    void add_edge(Vertex* vertex);
+    void increment_edge(Vertex* vertex);
     void calculate_probabilities();
 };
 
 class Graph {
 public:
-    map<string, Vertex *> vertices;
+    std::map<std::string, Vertex*> vertices;
 
-    Vertex * get_vertex(string value);
-    void add_edge(string v1, string v2);
+    Vertex* get_vertex(std::string value);
+    void add_edge(std::string v1, std::string v2);
     void generate_probability_mappings();
-    Vertex * get_next_word(Vertex * word);
+    Vertex* get_next_word(Vertex* word);
 };
-vector<string> get_words_from_text(string text_path);
-vector<string> get_files_from_directory(string directory_path);
-Graph make_graph(vector<string> words);
-vector<string> compose(Graph g, vector<string> words, int length=50);
 
 #endif
